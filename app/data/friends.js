261990df81d6c350bca62d82ffe3b4e538c5d8de
Helen Pitt <helenpitt@Helens-MacBook-Pro.local>
2018-02-09 12:27:49 -0500

    adding my friends

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friend survey information.
+// Initially we just set it equal to a "dummy" friend for testing.
+// ===============================================================================
+
+var friendArray = [{
+    "name": "Alex",
+    "photo": "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-1/p320x320/424733_10151477477248712_2031008508_n.jpg?oh=ccc88a12661cf05006d5f1f9dd447257&oe=5B0B5671",
+    "scores": [
+        "5",
+        "1",
+        "4",
+    ]
+},
+{
+    "name": "Sophie",
+    "photo": "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-1/p320x320/11863337_10102621419196766_1109911519899539716_n.jpg?oh=0f4df73992508fd523f6f195b522c479&oe=5B1FDF19",
+    "scores": [
+        "4",
+        "2",
+        "5",
+    ]
+},
+{
+    "name": "Tucker",
+    "photo": "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-1/c0.53.320.320/p320x320/27750712_10156128615444136_2007920776706885840_n.jpg?oh=61219cd0ca760db12dac9c6d8db9cdd8&oe=5B1F8B21",
+    "scores": [
+        "5",
+        "2",
+        "2",
+    ]
+},
+{
+    "name": "Al",
+    "photo": "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-1/p320x320/26804575_10155710450004733_7659035772513320558_n.jpg?oh=cd75958fd9e997c7ff0ef1f36f846bab&oe=5AD8901F",
+    "scores": [
+        "3",
+        "3",
+        "4",
+    ]
+},
+{
+    "name": "Semir",
+    "photo": "https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/1477954_10151860222363517_2020576655_n.jpg?oh=f77ea03c3c5daba4f5880da6629000d0&oe=5ADE08C6",
+    "scores": [
+        "4",
+        "3",
+        "4",
+    
+    ]
+},
+
+];
+
+// We export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
